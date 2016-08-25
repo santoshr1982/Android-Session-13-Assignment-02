b709027e4d4d72fec3a52a097a0acaba56221e57
santoshr1982 <santoshr1982@yahoo.com>
2016-08-25 10:56:16 +0530

    This program has been written to create menu using action bar.
    This program has been written to create menu using action bar.

---
+++ ActionBarAssignment/app/src/main/java/assignment/fragment/rsantosh/com/actionbarassignment/MainActivity.java
@@ -0,0 +1,24 @@
+package assignment.fragment.rsantosh.com.actionbarassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.fragment_menu,menu);
+
+        return true;
+    }
+}
